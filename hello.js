1fdb86c15781f72d7c198feda6a8856a1efbb101
Eric LEON <eleon@sqli.com>
2022-04-15 18:05:09 +0200

    handlebars

--- hello.js
+++ hello.js
@@ -1,7 +1,9 @@
 export const message = 'Hello!';
 
-const count = 0;
+const count = 99;
 
 export function hello() {
   console.log(message);
 }
+
+export default count;
--- server.js
+++ server.js
@@ -1,11 +1,42 @@
-import http from 'http';
+import express from 'express';
+import { create } from 'express-handlebars';
+
+import { ProductsController } from './controllers/products.controller.js';
+import { ProductsRepository } from './repository/products.repository.js';
+import { routes } from './routes/products.routes.js';
 
 const PORT = process.env.PORT || 3000;
+const TEMPLATING_EXT = '.hbs';
 
-const server = http.createServer((req, res) => {
-  res.end('Hello World!');
+const app = express();
+
+const hbs = create({ extname: TEMPLATING_EXT });
+app.engine(TEMPLATING_EXT, hbs.engine);
+app.set('view engine', TEMPLATING_EXT);
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use('/public', express.static('./assets'));
+
+const repository = new ProductsRepository();
+const controller = new ProductsController(repository);
+
+app.use('/', routes(controller));
+
+app.use((req, res) => {
+  console.log('Route error');
+  res.set('Content-Type', 'application/json')
+  res.status(404).json({ error: 'Page not found' });
 });
 
-server.listen(PORT, () => {
+app.use((error, req, res, next) => {
+  console.log('Fatal server error', error);
+  res.set('Content-Type', 'application/json')
+  res.status(500).json({ error: 'Fatal server error' });
+});
+
+app.listen(PORT, () => {
   console.log('Server listening on port', PORT);
 });
